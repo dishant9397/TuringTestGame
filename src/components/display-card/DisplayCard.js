d1035de5ef26df5cb5ff420f11d51ea78655a9dc
Dishant Patel <dishantpatel256@yahoo.in>
2022-10-23 16:38:00 -0700

    Display Robot Judge's decision

--- src/components/display-card/DisplayCard.js
+++ src/components/display-card/DisplayCard.js
@@ -10,12 +10,26 @@ const REFERENCE_SENTENCE =
     "property. (This can itself be the keyword auto, which sizes the flex item based on its contents.)";
 const HUMAN_TRANSLATION = "If the specified flex-basis is auto, the used flex basis is the value of the flex item’s main size " +
     "property. (This can itself be the keyword auto, which sizes the flex item based on its contents.)";
+const HUMAN_SCORE = 0.9
 const STATY_TRANSLATION = "If the specified flex-basis is auto, the used flex basis is the value of the flex item’s main size " +
     "property. (This can itself be the keyword auto, which sizes the flex item based on its contents.)";
+const STATY_SCORE = 0.8
 const NEURO_TRANSLATION = "If the specified flex-basis is auto, the used flex basis is the value of the flex item’s main size " +
     "property. (This can itself be the keyword auto, which sizes the flex item based on its contents.)";
+const NEURO_SCORE = 0.7
 
 function DisplayCard() {
+
+    const [humanScore, setHumanScore] = React.useState(-1)
+    const [statyScore, setStatyScore] = React.useState(-1)
+    const [neuroScore, setNeuroScore] = React.useState(-1)
+
+    const onClick = () => {
+        setHumanScore(HUMAN_SCORE)
+        setStatyScore(STATY_SCORE)
+        setNeuroScore(NEURO_SCORE)
+    }
+
     return (
         <Box sx={{ width: '100%' }} className="box">
             {<img src={Logo} alt="background" className="background" />}
@@ -45,14 +59,26 @@ function DisplayCard() {
                     </CardContent>
                     <CardActions>
                         {/*TODO: write the mobile view for portability*/}
-                        <Button style={{ textTransform: 'none' }} className="display-cardButton1" data-testid="humanButton">
-                            {HUMAN_TRANSLATION}
-                        </Button>
-                        <Button style={{ textTransform: 'none' }} className="display-cardButton2" data-testid="statyButton">
-                            {STATY_TRANSLATION}
-                        </Button>
-                        <Button style={{ textTransform: 'none' }} className="display-cardButton3" data-testid="neuroButton">
-                            {NEURO_TRANSLATION}
+                        <div style={{ display:'flex' }}>
+                            <Button style={{ textTransform: 'none' }} className="display-cardButton1" data-testid="humanButton">
+                                {HUMAN_TRANSLATION}
+                            </Button>
+                            {humanScore !== -1 && <Typography>{humanScore}</Typography>}
+                        </div>
+                        <div style={{ display:'flex' }}>
+                            <Button style={{ textTransform: 'none' }} className="display-cardButton2" data-testid="statyButton">
+                                {STATY_TRANSLATION}
+                            </Button>
+                            {statyScore !== -1 && <Typography>{statyScore}</Typography>}
+                        </div>
+                        <div style={{ display:'flex' }}>
+                            <Button style={{ textTransform: 'none' }} className="display-cardButton3" data-testid="neuroButton">
+                                {NEURO_TRANSLATION}
+                            </Button>
+                            {neuroScore !== -1 && <Typography>{neuroScore}</Typography>}
+                        </div>
+                        <Button onClick={onClick}>
+                            Get Scores
                         </Button>
                     </CardActions>
                 </Card>
