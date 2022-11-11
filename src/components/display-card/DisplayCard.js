aca938999b3e3c3e639f73bf586604a1a01bc6a5
Dishant Patel <dishantpatel256@yahoo.in>
2022-11-10 20:29:22 -0800

    TTG-52 Randomized Translation cards

--- src/components/display-card/DisplayCard.js
+++ src/components/display-card/DisplayCard.js
@@ -2,60 +2,71 @@
 import * as React from 'react';
 import { Box, Button, Card, CardContent, Typography, CardActions } from "@material-ui/core";
 import Logo from "../game-intro/resources/logo.svg";
-import StatyImg from "./resources/staty.png";
-import NeuroImg from "./resources/neuro.png";
-import HumanImg from "./resources/human.png";
+import StatyImage from "./resources/staty.png";
+import NeuroImage from "./resources/neuro.png";
+import HumanImage from "./resources/human.png";
+import Translation from './Translation';
 
 function DisplayCard(props) {
 
-    const HUMAN = "human";
-    const STATY = "staty";
-    const NEURO = "neuro";
+    const HUMAN = "HUMAN"
+    const STATISTICAL = "STATISTICAL"
+    const NEURO = "NEURO"
+
     const card = props?.card
+    const {choice, setChoice} = props;
+    const {score, setScore} = props
+    const order = props?.order
 
     const trimScore = (score) => {
         return (Math.trunc(score * Math.pow(10, 4)) / Math.pow(10, 4))
     }
 
-    const humanScore = trimScore(card.humanScore)
-    const statyScore = trimScore(card.statisticalMachineScore)
-    const neuroScore = trimScore(card.neuralMachineScore)
+    const options = {
+        score: score,
+        choice: choice,
+        setChoice: setChoice
+    }
 
-    const {choice, setChoice} = props;
-    const {showScore, setShowScore} = props
-    const {score, setScore} = props
+    const translations = [
+        {
+            type: HUMAN,
+            translation: card.humanTranslation,
+            score: trimScore(card.humanScore),
+            image: HumanImage,
+            isSelected: false
+        },
+        {
+            type: STATISTICAL,
+            translation: card.statisticalMachineTranslation,
+            score: trimScore(card.statisticalMachineScore),
+            image: StatyImage,
+            isSelected: false
+        },
+        {
+            type: NEURO,
+            translation: card.neuralMachineTranslation,
+            score: trimScore(card.neuralMachineScore),
+            image: NeuroImage,
+            isSelected: false
+        }
+    ]
 
     const calculateScore = () => {
         if (choice === HUMAN) {
             setScore(score => ({...score, player: score.player + 1}))
         }
-        if (humanScore > statyScore && humanScore > neuroScore) {
+        if (translations[0].score > translations[1].score && translations[0].score > translations[2].score) {
             setScore(score => ({...score, robot: score.robot + 1}))
         }
     }
     
     const onSubmit = () => {
-        if (!showScore) {
+        if (!score.enable) {
             calculateScore()
-            console.log(score)
-            setShowScore(true)
+            setScore(score => ({...score, enable: true}))
         }
     }
-
-    const handleClick = (id) => {
-        if (!showScore) {
-            if (choice !== id) {
-                setChoice(id);
-            } else {
-                setChoice("");
-                setShowScore(false)
-            }
-        }
-    }
-
-    const currentChoice1 = choice === HUMAN ? "display-cardButton1-clicked" : "display-cardButton1";
-    const currentChoice2 = choice === STATY ? "display-cardButton2-clicked" : "display-cardButton2";
-    const currentChoice3 = choice === NEURO ? "display-cardButton3-clicked" : "display-cardButton3";
 
     return (
         <Box sx={{ width: '100%' }} className="box">
@@ -66,7 +77,7 @@ function DisplayCard(props) {
                 </Typography>
             </div>
             <div className="display-questionZone">
-                <Card style={{maxHeight: '60vh', overflow: 'auto'}}>
+                <Card style={{maxHeight: '70vh', overflow: 'auto'}}>
                     <CardContent data-testid="questionZone">
                         <Typography gutterBottom variant="h5" component="div">
                             The original sentence:
@@ -84,34 +95,16 @@ function DisplayCard(props) {
                     </CardContent>
                     <CardActions style={{ alignItems: 'flex-start', marginLeft: '4%' }}>
                         {/*TODO: write the mobile view for portability*/}
-                        <div style={{ display:'flex' }}>
-                            <Button style={{ textTransform: 'none', justifyContent:'flex-start' }} className={currentChoice1} data-testid="humanButton" onClick={() => handleClick(HUMAN)}>
-                                {card.humanTranslation}
-                            </Button>
-                            {showScore && <Typography data-testid="humanScore" className="score-class">{humanScore}</Typography>}
-                            {showScore && <img src={HumanImg} data-testid="humanIdentityImg" alt={HUMAN} title={HUMAN} className="identityImg" />}
-                        </div>
-                        <div style={{ display:'flex' }}>
-                            <Button style={{ textTransform: 'none', justifyContent:'flex-start' }} className={currentChoice2} data-testid="statyButton" onClick={() => handleClick(STATY)}>
-                                {card.statisticalMachineTranslation}
-                            </Button>
-                            {showScore && <Typography data-testid="statyScore" className="score-class">{statyScore}</Typography>}
-                            {showScore && <img src={StatyImg} data-testid="statyIdentityImg" alt={STATY} title={STATY} className="identityImg" />}
-                        </div>
-                        <div style={{ display:'flex' }}>
-                            <Button style={{ textTransform: 'none', justifyContent:'flex-start' }} className={currentChoice3} data-testid="neuroButton" onClick={() => handleClick(NEURO)}>
-                                {card.neuralMachineTranslation}
-                            </Button>
-                            {showScore && <Typography data-testid="neuroScore" className="score-class">{neuroScore}</Typography>}
-                            {showScore && <img src={NeuroImg} data-testid="neuroIdentityImg" alt={NEURO} title={NEURO} className="identityImg" />}
-                        </div>
+                        <Translation details={translations[order[0]]} options={options}/>
+                        <Translation details={translations[order[1]]} options={options}/>
+                        <Translation details={translations[order[2]]} options={options}/>
                         {choice !== "" && <Button className="submit-button" data-testid="submitBtn" onClick={onSubmit}>Submit</Button>}
                     </CardActions>
                 </Card>
                 <div style={{ display:'flex' }}>
-                    {showScore && <Typography variant="h6" className="score-typography" data-testid="playerScore" align="left">Player's Score: {score.player}</Typography>}
-                    {showScore && <Typography style={{width: '40%'}}></Typography>}
-                    {showScore && <Typography variant="h6" className="score-typography" data-testid="robotScore" align="right">Robot's Score: {score.robot}</Typography>}
+                    {score.enable && <Typography variant="h6" className="score-typography" data-testid="playerScore" align="left">Player's Score: {score.player}</Typography>}
+                    {score.enable && <Typography style={{width: '40%'}}></Typography>}
+                    {score.enable && <Typography variant="h6" className="score-typography" data-testid="robotScore" align="right">Robot's Score: {score.robot}</Typography>}
                 </div>
             </div>
         </Box>
