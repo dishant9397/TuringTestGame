65d3976a54a4b57d27df3b40aaea80d41f274a00
Dishant Patel <dishantpatel256@yahoo.in>
2022-11-29 21:35:43 -0800

    TTG-70 Removed commented lines

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-// AIzaSyCtEH8HVvABGTM3IrNpEXm5NpfVOIvkTd8
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
